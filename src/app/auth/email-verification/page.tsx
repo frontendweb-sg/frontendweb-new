5f49a162a38ca593f64cc3aef212b5405a89abf3
startx88 <arya.creativemind@gmail.com>
2023-05-31 13:05:05 +0530

    update auth folder

--- src/app/auth/email-verification/page.tsx
+++ src/app/auth/email-verification/page.tsx
@@ -1,17 +1,34 @@
 "use client";
+import Button from "@/components/ui/Button";
+import Typography from "@/components/ui/Typography";
+import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import { BsCheck } from "react-icons/bs";
 
 /**
  * Email verification component
  * @param param0
  * @returns
  */
-const Page = ({ params }: { params: { success: boolean } }) => {
+const Page = () => {
   const searchParams = useSearchParams();
   const success = searchParams.get("success");
 
   if (success === "true") {
-    return <div>Email verified success</div>;
+    return (
+      <div className="text-center gap-4 d-flex flex-column justify-content-center align-items-center">
+        <div
+          className="d-flex justify-content-center fs-1 align-items-center border-border rounded-circle"
+          style={{ width: "50px", height: "50px" }}
+        >
+          <BsCheck className=" text-success " />
+        </div>
+        <Typography>You have successfully verified your account</Typography>
+        <Link className="btn btn-primary btn-md" href="/auth">
+          Login
+        </Link>
+      </div>
+    );
   }
 
   return <div>Email verified</div>;
--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -6,28 +6,7 @@ import Row from "@/components/ui/Row";
 import Typography from "@/components/ui/Typography";
 
 const Page = () => {
-  return (
-    <Container isFull cover>
-      <Row cover>
-        <Col className="bg-primary text-light align-items-center justify-content-center d-flex">
-          <div className="w-50">
-            <Logo className="mb-5" href="/" color="light" />
-            <Typography>Welcome to sign in</Typography>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
-            eius sint, voluptatem nesciunt, laborum placeat sunt culpa magni
-            expedita facilis aut iste amet, reprehenderit consequatur voluptate
-            ullam veniam ipsam quae!
-            <Typography variant="h5" className="mt-4">
-              Learn frontend technologies by expert
-            </Typography>
-          </div>
-        </Col>
-        <Col className="justify-content-center align-items-center d-flex relative">
-          <Auth />
-        </Col>
-      </Row>
-    </Container>
-  );
+  return <Auth />;
 };
 
 export default Page;
