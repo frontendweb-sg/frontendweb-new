4f0a7edc409ce8eaf359c0cb9bc77e53711bcb61
startx88 <arya.creativemind@gmail.com>
2023-05-31 13:29:59 +0530

    add container test

---
+++ src/__tests__/components/container.test.tsx
@@ -0,0 +1,20 @@
+import Container from "@/components/ui/Container";
+import type { ContainerProps } from "@/components/ui/Container";
+import { render, screen } from "../../test-util";
+
+const setup = (props?: ContainerProps) =>
+	render(<Container {...props}>test</Container>);
+
+describe("Container", () => {
+	it("Render component with default props", () => {
+		setup();
+		const text = screen.getByText("test");
+		expect(text).toBeInTheDocument();
+	});
+
+	it("Render component with custom props", () => {
+		setup({ isFull: true });
+		const text = screen.getByText("test");
+		expect(text.classList[0]).toContain("container-fluid");
+	});
+});
